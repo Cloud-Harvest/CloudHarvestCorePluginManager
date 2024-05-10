e9285570af6127f5790061a161f96bde0acadba3
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-05-10 17:53:22 -0400

    moved setup metadata

---
+++ CloudHarvestCorePluginManager/meta.py
@@ -0,0 +1,11 @@
+meta = dict(name='CloudHarvestCorePluginManager',
+            version='0.1.0',
+            description='This module is responsible for managing plugins and their configurations.'
+                        ' It is designed to work with both the Cloud Harvest API and the Cloud Harvest CLI to load'
+                        ' components from plugins such as the Cloud Harvest AWS Plugin.',
+            author='Cloud Harvest, Fiona June Leathers',
+            url='https://github.com/Cloud-Harvest/CloudHarvestCorePluginManager',
+            classifiers=[
+                'Programming Language :: Python :: 3.12',
+            ],
+            license='CC Attribution-NonCommercial-ShareAlike 4.0 International')
